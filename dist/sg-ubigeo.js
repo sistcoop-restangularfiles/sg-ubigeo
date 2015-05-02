254e62a444cad5f14520f35bfe947d0cfda38813
carlos <carlosthe19916@gmail.com>
2015-05-02 12:57:38 -0500

    SGUbigeo

--- dist/sg-ubigeo.js
+++ dist/sg-ubigeo.js
@@ -28,96 +28,22 @@
         });
     }]);
 
-    module.factory('SGDepartamento', ['UbigeoRestangular',  function(UbigeoRestangular) {
+    module.factory('SGUbigeo', ['UbigeoRestangular',  function(UbigeoRestangular) {
 
         var url = 'departamentos';
         var urlCount = url + '/count';
 
         var modelMethos = {
-            $new: function(id){
-                return angular.extend({id: id}, modelMethos);
-            },
-            $build: function(){
-                return angular.extend({id: undefined}, modelMethos, {$save: function(){
-                    return UbigeoRestangular.all(url).post(this);
-                }});
-            },
-            $save: function() {
-                return UbigeoRestangular.one(url, this.id).customPUT(UbigeoRestangular.copy(this),'',{},{});
-            },
-
-            $find: function(id){
-                return UbigeoRestangular.one(url, id).get();
+            $find: function(ubigeo){
+                return UbigeoRestangular.one(url, ubigeo).get();
             },
             $search: function(queryParams){
                 return UbigeoRestangular.all(url).getList(queryParams);
-            },
-
-            $count: function(){
-                return UbigeoRestangular.one(urlCount).get();
-            },
-
-            $disable: function(){
-                return UbigeoRestangular.all(url+'/'+this.id+'/disable').post();
-            },
-            $remove: function(id){
-                return UbigeoRestangular.one(url, id).remove();
             },
 
             $getProvincias: function(){
-                return UbigeoRestangular.all(url+'/'+this.codigo+'/provincias').getList();
-            }
-
-        };
-
-        UbigeoRestangular.extendModel(url, function(obj) {
-            if(angular.isObject(obj)) {
-                return angular.extend(obj, modelMethos);
-            } else {
-                return angular.extend({id: obj}, modelMethos)
-            }
-        });
-
-        return modelMethos;
-
-    }]);
-
-    module.factory('SGProvincia', ['UbigeoRestangular',  function(UbigeoRestangular) {
-
-        var url = 'provincias';
-        var urlCount = url + '/count';
-
-        var modelMethos = {
-            $new: function(id){
-                return angular.extend({id: id}, modelMethos);
+                return UbigeoRestangular.all(url+'/'+this.ubigeoDepartamento+'/provincias').getList();
             },
-            $build: function(){
-                return angular.extend({id: undefined}, modelMethos, {$save: function(){
-                    return UbigeoRestangular.all(url).post(this);
-                }});
-            },
-            $save: function() {
-                return UbigeoRestangular.one(url, this.id).customPUT(UbigeoRestangular.copy(this),'',{},{});
-            },
-
-            $find: function(id){
-                return UbigeoRestangular.one(url, id).get();
-            },
-            $search: function(queryParams){
-                return UbigeoRestangular.all(url).getList(queryParams);
-            },
-
-            $count: function(){
-                return UbigeoRestangular.one(urlCount).get();
-            },
-
-            $disable: function(){
-                return UbigeoRestangular.all(url+'/'+this.id+'/disable').post();
-            },
-            $remove: function(id){
-                return UbigeoRestangular.one(url, id).remove();
-            },
-
             $getDistritos: function(codigoDepartamento, codigoProvincia){
                 return UbigeoRestangular.all('departamentos/'+codigoDepartamento+'/'+url+'/'+codigoProvincia+'/distritos').getList();
             }
@@ -136,53 +62,5 @@
 
     }]);
 
-    module.factory('SGDistrito', ['UbigeoRestangular',  function(UbigeoRestangular) {
-
-        var url = 'distritos';
-        var urlCount = url + '/count';
-
-        var modelMethos = {
-            $new: function(id){
-                return angular.extend({id: id}, modelMethos);
-            },
-            $build: function(){
-                return angular.extend({id: undefined}, modelMethos, {$save: function(){
-                    return UbigeoRestangular.all(url).post(this);
-                }});
-            },
-            $save: function() {
-                return UbigeoRestangular.one(url, this.id).customPUT(UbigeoRestangular.copy(this),'',{},{});
-            },
-
-            $find: function(id){
-                return UbigeoRestangular.one(url, id).get();
-            },
-            $search: function(queryParams){
-                return UbigeoRestangular.all(url).getList(queryParams);
-            },
-
-            $count: function(){
-                return UbigeoRestangular.one(urlCount).get();
-            },
-
-            $disable: function(){
-                return UbigeoRestangular.all(url+'/'+this.id+'/disable').post();
-            },
-            $remove: function(id){
-                return UbigeoRestangular.one(url, id).remove();
-            }
-        };
-
-        UbigeoRestangular.extendModel(url, function(obj) {
-            if(angular.isObject(obj)) {
-                return angular.extend(obj, modelMethos);
-            } else {
-                return angular.extend({id: obj}, modelMethos)
-            }
-        });
-
-        return modelMethos;
-
-    }]);
 
 })();
