0a2c3f9528bc00cf937c11c03325c2c8aa9fb2b5
Carlos <carlosthe19916@gmail.com>
2015-08-19 10:47:37 -0500

    SHUBIGEO.SEARCH

--- dist/sg-ubigeo.js
+++ dist/sg-ubigeo.js
@@ -1,66 +1,120 @@
 'use strict';
 
-(function(){
+(function () {
 
     var module = angular.module('sg-ubigeo', ['restangular']);
 
-    module.provider('sgUbigeo', function() {
+    module.provider('sgUbigeo', function () {
 
         this.restUrl = 'http://localhost';
 
-        this.$get = function() {
+        this.$get = function () {
             var restUrl = this.restUrl;
             return {
-                getRestUrl: function() {
+                getRestUrl: function () {
                     return restUrl;
                 }
             }
         };
 
-        this.setRestUrl = function(restUrl) {
+        this.setRestUrl = function (restUrl) {
             this.restUrl = restUrl;
         };
     });
 
-    module.factory('UbigeoRestangular', ['Restangular', 'sgUbigeo', function(Restangular, sgUbigeo) {
-        return Restangular.withConfig(function(RestangularConfigurer) {
+    module.factory('UbigeoRestangular', ['Restangular', 'sgUbigeo', function (Restangular, sgUbigeo) {
+        return Restangular.withConfig(function (RestangularConfigurer) {
             RestangularConfigurer.setBaseUrl(sgUbigeo.getRestUrl());
         });
     }]);
 
-    module.factory('SGUbigeo', ['UbigeoRestangular',  function(UbigeoRestangular) {
+    var RestObject = function (path, restangular, extendMethods) {
+        var modelMethods = {
 
-        var url = 'departamentos';
-        var urlCount = url + '/count';
-
-        var modelMethos = {
-            $find: function(ubigeo){
-                return UbigeoRestangular.one(url, ubigeo).get();
-            },
-            $search: function(queryParams){
-                return UbigeoRestangular.all(url).getList(queryParams);
+            /**
+             * Retorna url*/
+            $getModelMethods: function () {
+                return modelMethods;
             },
 
-            $getProvincias: function(){
-                return UbigeoRestangular.all(url+'/'+this.ubigeoDepartamento+'/provincias').getList();
+            /**
+             * Retorna url*/
+            $getBasePath: function () {
+                return path;
             },
-            $getDistritos: function(codigoDepartamento, codigoProvincia){
-                return UbigeoRestangular.all('departamentos/'+codigoDepartamento+'/provincias/'+codigoProvincia+'/distritos').getList();
+            /**
+             * Retorna la url completa del objeto*/
+            $getAbsoluteUrl: function () {
+                return restangular.one(path, this.id).getRestangularUrl();
+            },
+            /**
+             * Concatena la url de subresource con la url base y la retorna*/
+            $concatSubResourcePath: function (subResourcePath) {
+                return this.$getBasePath() + '/' + this.id + '/' + subResourcePath;
+            },
+
+
+            $new: function (id) {
+                return angular.extend({id: id}, modelMethods);
+            },
+            $build: function () {
+                return angular.extend({id: undefined}, modelMethods, {
+                    $save: function () {
+                        return restangular.all(path).post(this);
+                    }
+                });
+            },
+
+            $search: function (queryParams) {
+                return restangular.all(path).customGET('', queryParams);
+            },
+
+            $find: function (id) {
+                return restangular.one(path, id).get();
+            },
+            $save: function () {
+                return restangular.one(path, this.id).customPUT(restangular.copy(this), '', {}, {});
+            },
+            $saveSent: function (obj) {
+                return restangular.all(path).post(obj);
+            },
+
+            $enable: function () {
+                return restangular.one(path, this.id).all('enable').post();
+            },
+            $disable: function () {
+                return restangular.one(path, this.id).all('disable').post();
+            },
+            $remove: function () {
+                return restangular.one(path, this.id).remove();
             }
-
         };
 
-        UbigeoRestangular.extendModel(url, function(obj) {
-            if(angular.isObject(obj)) {
-                return angular.extend(obj, modelMethos);
+        modelMethods = angular.extend(modelMethods, extendMethods);
+
+        restangular.extendModel(path, function (obj) {
+            if (angular.isObject(obj)) {
+                return angular.extend(obj, modelMethods);
             } else {
-                return angular.extend({id: obj}, modelMethos)
+                return angular.extend({id: obj}, modelMethods)
             }
         });
 
-        return modelMethos;
+        restangular.extendCollection(path, function (collection) {
+            angular.forEach(collection, function (row) {
+                angular.extend(row, modelMethods);
+            });
+            return collection;
+        });
 
+        return modelMethods;
+    };
+
+    module.factory('SGUbigeo', ['UbigeoRestangular', function (UbigeoRestangular) {
+        var extendMethod = {};
+
+        var ubigeoResource = RestObject('ubigeos', UbigeoRestangular, extendMethod);
+        return ubigeoResource;
     }]);
-
 
 })();
